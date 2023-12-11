3f7f78fbec8e6a88202d74870cc21be594027276
asanmor916 <asanmor916@iesalmudeyne.es>
2023-12-11 22:02:57 +0100

    implemento en rama2 boton con alert

--- js.js
+++ js.js
@@ -1,3 +1,10 @@
 addEventListener("DOMContentLoaded", function (){
 alert("hola");
 });
+
+btn= document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+
+alert("todavia queda implementar el div con el contenido al hacerme click");
+});
